58ede4a6d75d1432026ccacc935462622da8e767
mohammadanang <m.anangnur@gmail.com>
2019-10-25 19:46:11 +0700

    remanage index action & response core

--- index.js
+++ index.js
@@ -1,60 +1,7 @@
-class API {
-    constructor(model) {
-        this.model = model
-    }
+const action = require("./actions/index.action")
+const response = require("./responses/index.response")
 
-    async list() {
-        try {
-            let data = await this.model.find({}).exec()
-
-            return data
-        } catch(err) {
-            throw err
-        }
-    }
-
-    async show(q) {
-        try {
-            let data = await this.model.findOne(q).exec()
-
-            return data
-        } catch(err) {
-            throw err
-        }
-    }
-
-    async create(input) {
-        try {
-            let data = new this.model(input)
-            await data.save()
-
-            return data
-        } catch(err) {
-            throw err
-        }
-    }
-
-    async update(q, input, opts) {
-        try {
-            let data = await this.model.findOneAndUpdate(
-                q, input, opts
-            ).exec()
-
-            return data
-        } catch(err) {
-            throw err
-        }
-    }
-
-    async delete(q) {
-        try {
-            let data = await this.model.findOneAndDelete(q).exec()
-
-            return data
-        } catch(err) {
-            throw err
-        }
-    }
+module.exports = {
+    Api: action,
+    response
 }
-
-module.exports = API
