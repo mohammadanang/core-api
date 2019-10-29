bc4e969d80a78da2419107612b43d055fafd9ea2
hndryno <hendriyono97@gmail.com>
2019-10-29 20:39:09 +0700

    add populate on list

--- actions/index.action.js
+++ actions/index.action.js
@@ -1,62 +1,76 @@
 class API {
-  constructor(model) {
-    this.model = model;
+  constructor(model, model2) {
+      this.model = model
+      this.model2 = model2
   }
 
-  async list(q) {
-    try {
-      let params = {};
-      if (q) {
-        params = q;
+  async list(q, request_path) {
+      try {
+          let params = {}
+
+          let populate_params = []
+
+          if(request_path == []){
+              populate_params = []
+          }else{
+              populate_params = {path: request_path, model: this.model2}
+          }
+
+          if(q) {
+              params = q
+          }
+
+          let data = await this.model.find(params).populate(
+              populate_params    
+          ).exec()
+
+          return data
+      } catch(err) {
+          throw err
       }
-
-      let data = await this.model.find(params).exec();
-
-      return data;
-    } catch (err) {
-      throw err;
-    }
   }
 
   async show(q) {
-    try {
-      let data = await this.model.findOne(q).exec();
+      try {
+          let data = await this.model.findOne(q).exec()
 
-      return data;
-    } catch (err) {
-      throw err;
-    }
+          return data
+      } catch(err) {
+          throw err
+      }
   }
 
   async create(input) {
-    try {
-      let data = new this.model(input);
-      await data.save();
+      try {
+          let data = new this.model(input)
+          await data.save()
 
-      return data;
-    } catch (err) {
-      throw err;
-    }
+          return data
+      } catch(err) {
+          throw err
+      }
   }
 
   async update(q, input, opts) {
-    try {
-      let data = await this.model.findOneAndUpdate(q, input, opts).exec();
+      try {
+          let data = await this.model.findOneAndUpdate(
+              q, input, opts
+          ).exec()
 
-      return data;
-    } catch (err) {
-      throw err;
-    }
+          return data
+      } catch(err) {
+          throw err
+      }
   }
 
   async delete(q) {
-    try {
-      let data = await this.model.findOneAndDelete(q).exec();
+      try {
+          let data = await this.model.findOneAndDelete(q).exec()
 
-      return data;
-    } catch (err) {
-      throw err;
-    }
+          return data
+      } catch(err) {
+          throw err
+      }
   }
 
   async softDelete(q) {
