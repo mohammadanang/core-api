0818407cc676d038d1f3f736c73435c75b5ffd98
mohammadanang <m.anangnur@gmail.com>
2020-03-04 17:03:20 +0700

    add mongodb connection

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const action = require("./actions/index.action")
 const response = require("./responses/index.response")
 const Controller = require("./controllers/index.controller")
 const LogListener = require("./listeners/log.listener")
+const mongoose = require("mongoose")
+const connection = host => {
+    mongoose.connect(host, {
+        'useNewUrlParser': true
+    })
+
+    mongoose.set('useCreateIndex', true)
+}
 
 module.exports = {
     Api: action,
     response,
     Controller,
-    LogListener
+    LogListener,
+    connection
 }
