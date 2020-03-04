2af215c033f6e1bf03602f7225bbeba8380f1abd
mohammadanang <m.anangnur@gmail.com>
2020-03-04 12:23:21 +0700

    add comment to import listener

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const action = require("./actions/index.action")
 const response = require("./responses/index.response")
 const Controller = require("./controllers/index.controller")
-const LogListener = require("./listeners/log.listener")
+const LogListener = require("./listeners/log.listener") // need event emitter imported
 
 module.exports = {
     Api: action,
