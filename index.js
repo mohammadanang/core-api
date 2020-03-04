7053e6e5113792a4deffcbf64289ccd45e3c2b15
mohammadanang <m.anangnur@gmail.com>
2020-03-04 14:24:26 +0700

    remove event emitter imported

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const action = require("./actions/index.action")
 const response = require("./responses/index.response")
 const Controller = require("./controllers/index.controller")
-const events = require("events")
-const eventEmitter = new events.EventEmitter()
-const LogListener = require("./listeners/log.listener")(eventEmitter) // need event emitter imported
+const LogListener = require("./listeners/log.listener")
 
 module.exports = {
     Api: action,
     response,
     Controller,
-    LogListener,
-    eventEmitter
+    LogListener
 }
