8f71f598527221e0c8361d6804ff7815d0095f2a
mohammadanang <m.anangnur@gmail.com>
2020-03-04 13:04:21 +0700

    add event emitter to export

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const action = require("./actions/index.action")
 const response = require("./responses/index.response")
 const Controller = require("./controllers/index.controller")
-const LogListener = require("./listeners/log.listener") // need event emitter imported
+const events = require("events")
+const eventEmitter = new events.EventEmitter()
+const LogListener = require("./listeners/log.listener")(eventEmitter) // need event emitter imported
 
 module.exports = {
     Api: action,
     response,
     Controller,
-    LogListener: LogListener(eventEmitter)
+    LogListener,
+    eventEmitter
 }
